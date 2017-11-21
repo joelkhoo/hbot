5e90445bf7aa5180074ae9024021c33aff76aff3
Joel Khoo <joelkhoo@gmail.com>
2017-11-21 14:10:39 +0800

    Fix api-ai module issues and add chat-dialog components

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,8 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  dialogflow: {
+    angularBot: 'ce065c16522a4ff7834c1c3eafaf7594'
+  }
 };
